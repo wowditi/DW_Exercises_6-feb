eee8974327ad7b8bc65c769b4d0aaf9f1c51c7f8
VosVeinard <reinardbeekman@gmail.com>
2015-02-18 16:26:19 +0100

    Graphs

--- ColorRefine.py
+++ ColorRefine.py
@@ -3,27 +3,31 @@ from basicgraphs import graph
 
 
 def sortlist(_l):
-	array = []
-	for i in range(len(_l)):
+	array = [len(_l)+1]
+	for i in range(len(_l)-1):
 		array[i] = [len(_l[i].V()), len(_l[i].E())]
-	returnarray = [[]]
-	for i in range(array):
+	returnarray = [[] for x in range(len(_l))]
+	print(array)
+	for i in range(len(array)-1):
 		for g in array:
 			if g[0] == array[i][0] and g[1] == array[i][1]:
-				returnarray[i] += _l[i]
+				returnarray[i].append(_l[i].V())
 	print(returnarray)
 
 G = loadgraph('examplegraph.gr')
 H = loadgraph('examplegraph2.gr')
 blaat = [G, H]
-print(sortlist(blaat))
 
 
 def colorrefine(_l):
+	degarray = [[] for x in range(len(_l[0].V()))]
 	for elem in _l:
 		for v in elem.V():
+			print(v.deg())
 			v.colornum = v.deg()
+			degarray[v.deg()].append(v)
 	i = 0
+	print(degarray)
 	notfinished = True
-	while notfinished:
-		i += 1
+
+colorrefine(blaat)
