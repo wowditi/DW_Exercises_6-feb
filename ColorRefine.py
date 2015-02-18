8f5a56137fe0d94f70c9f763533a09aa70f99cda
wowditi <niels_geuze@hotmail.com>
2015-02-18 16:25:31 +0100

    initialcheck

--- ColorRefine.py
+++ ColorRefine.py
@@ -5,17 +5,30 @@ from basicgraphs import graph
 def sortlist(_l):
 	array = []
 	for i in range(len(_l)):
-		array[i] = [len(_l[i].V()), len(_l[i].E())]
-	returnarray = [[]]
-	for i in range(array):
-		for g in array:
-			if g[0] == array[i][0] and g[1] == array[i][1]:
-				returnarray[i] += _l[i]
+		array.append([len(_l[i].V()), len(_l[i].E())])
+	print(array)
+	returnarray = []
+	i = 0
+	temp = 1
+	while i < len(array)-1: #for i in range(len(array)-1):
+		temparray = []
+		temparray.append(_l[i])
+		for g in range(i+1,len(array)):
+			if array[g][0] == array[i][0] and array[g][1] == array[i][1]:
+				temparray.append(_l[g])
+				temp += 1
+		returnarray.append(temparray)
+		i += temp
+		temp = 0
 	print(returnarray)
 
 G = loadgraph('examplegraph.gr')
-H = loadgraph('examplegraph2.gr')
-blaat = [G, H]
+H = loadgraph('examplegraph.gr')
+J = loadgraph('examplegraph.gr')
+T = loadgraph('examplegraph2.gr')
+Q = loadgraph('examplegraph2.gr')
+
+blaat = [G, H, J, T, Q]
 print(sortlist(blaat))
 
 
